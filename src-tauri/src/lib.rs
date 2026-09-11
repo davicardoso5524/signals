222161936ac5e027db1d20d9208b77ab4451948d
Davi Cardoso <davicardoso524@academico.ufs.br>
2026-09-10 21:27:19 -0300

    Keep SIGNALS running in the Windows tray

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,47 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
+    .setup(|app| {
+      use tauri::{menu::{Menu, MenuItem}, tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent}, Manager};
+
+      let open = MenuItem::with_id(app, "open", "Open SIGNALS", true, None::<&str>)?;
+      let quit = MenuItem::with_id(app, "quit", "Quit SIGNALS", true, None::<&str>)?;
+      let menu = Menu::with_items(app, &[&open, &quit])?;
+
+      TrayIconBuilder::new()
+        .icon(app.default_window_icon().cloned().expect("SIGNALS default icon is configured"))
+        .menu(&menu)
+        .tooltip("SIGNALS")
+        .on_menu_event(|app, event| match event.id.as_ref() {
+          "open" => {
+            if let Some(window) = app.get_webview_window("main") {
+              let _ = window.show();
+              let _ = window.unminimize();
+              let _ = window.set_focus();
+            }
+          }
+          "quit" => app.exit(0),
+          _ => {}
+        })
+        .on_tray_icon_event(|tray, event| {
+          if let TrayIconEvent::Click { button: MouseButton::Left, button_state: MouseButtonState::Up, .. } = event {
+            if let Some(window) = tray.app_handle().get_webview_window("main") {
+              let _ = window.show();
+              let _ = window.unminimize();
+              let _ = window.set_focus();
+            }
+          }
+        })
+        .build(app)?;
+
+      Ok(())
+    })
+    .on_window_event(|window, event| {
+      if let tauri::WindowEvent::CloseRequested { api, .. } = event {
+        api.prevent_close();
+        let _ = window.hide();
+      }
+    })
     .plugin(tauri_plugin_opener::init())
     .plugin(tauri_plugin_process::init())
     .plugin(tauri_plugin_updater::Builder::new().build())
